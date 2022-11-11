06aefd58ade7a680f1cc6eb433f9aea1602105d5
briannlz <briannlongzhao@gmail.com>
2022-11-10 17:55:06 -0800

    fix custom stem

--- centernet/modeling/backbone/res2net.py
+++ centernet/modeling/backbone/res2net.py
@@ -23,7 +23,6 @@ from detectron2.modeling.backbone.build import BACKBONE_REGISTRY
 from .fpn_p5 import LastLevelP6P7_P5
 from .bifpn import BiFPN
 
-from .custom_stem import CustomStem
 
 __all__ = [
     "ResNetBlockBase",
@@ -565,6 +564,45 @@ class BasicStem(CNNBlockBase):
         x = self.bn1(x)
         x = F.relu_(x)
         x = F.max_pool2d(x, kernel_size=3, stride=2, padding=1)
+        return x
+
+
+class CustomStem(BasicStem):
+    """
+    The custom ResNet stem (layers before the first residual block).
+    """
+    def __init__(self, in_channels=3, out_channels=64, norm="BN"):
+        """
+        Args:
+            norm (str or callable): norm after the first conv layer.
+                See :func:`layers.get_norm` for supported format.
+        """
+        super().__init__(in_channels, out_channels, norm)
+
+    def quantize(self, x, k, do_quantise=True):
+        xmax = torch.max(x)
+        xmin = torch.min(x)
+        if xmax < -xmin:
+            xmax = -xmin
+        if not do_quantise:
+            return x
+        digital = torch.round(((2 ** k) - 1) * x / xmax)
+        output = xmax * digital / ((2 ** k) - 1)
+        return output
+
+    def noise(self, x, mean=0.0, std=0.01):
+        f = torch.normal(torch.full(x.size(), mean), std).to(x.device)
+        x += x * torch.max(x) * f
+        return x
+
+    def forward(self, x):
+        x = self.conv1(x)  # Change this to custom_conv
+        x = self.bn1(x)
+        x = F.relu_(x)
+        x = F.max_pool2d(x, kernel_size=3, stride=2, padding=1)
+        # Add noise and quantize
+        x = self.noise(x, std=0.01)
+        x = self.quantize(x, k=8)
         return x
 
 
